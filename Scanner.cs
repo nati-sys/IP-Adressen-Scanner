561fe14d63300e46038536f84a226f1bb0c246e7
Aigner Natalia <N.Aigner@htlvb.at>
2026-04-10 08:55:38 +0200

    Add ping function

--- Scanner.cs
+++ Scanner.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using System.Text;
 
 namespace IP_Adressen_Scanner
 {
     internal class Scanner
     {
+        public bool PingHost(string ip)
+        {
+            try
+            {
+                Ping ping = new Ping();
+                PingReply reply = ping.Send(ip, 1000);
+                return reply.Status == IPStatus.Success;
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
