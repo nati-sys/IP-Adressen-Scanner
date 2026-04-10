15e9486908e3edba9c3def15eeef82138d7305b9
Feichtinger Fabian <F.Feichtinger@htlvb.at>
2026-04-10 08:59:58 +0200

    checking if port is open

--- PortChecker.cs
+++ PortChecker.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using System.Text;
 
 namespace IP_Adressen_Scanner
 {
-    internal class PortChecker
+    public class PortChecker
     {
-
+        public bool IsPortOpen(string ip, int port)
+        {
+            try
+            {
+                using TcpClient client = new TcpClient();
+                var result = client.BeginConnect(ip, port, null, null);
+                bool success = result.AsyncWaitHandle.WaitOne(1000);
+                client.EndConnect(result);
+                return success;
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
